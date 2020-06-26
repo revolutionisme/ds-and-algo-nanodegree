a265a931a3af6af0998dcc5a34c32d8cb95d93d4
Biswas Biplob <Biplob.Biswas@burda-forward.de>
2020-06-27 01:13:34 +0200

    FInish task 2

--- P0-Unscramble-Computer=Science-Problems/Task2.py
+++ P0-Unscramble-Computer=Science-Problems/Task2.py
@@ -20,3 +20,18 @@ Print a message:
 September 2016.".
 """
 
+time_spent_dict = {}
+
+for row in calls:
+    if row[0] not in time_spent_dict:
+        time_spent_dict[row[0]] = int(row[3])
+    else:
+        time_spent_dict[row[0]] += int(row[3])
+    
+    if row[1] not in time_spent_dict:
+        time_spent_dict[row[1]] = int(row[3])
+    else:
+        time_spent_dict[row[1]] += int(row[3])
+
+number, total_time = max(time_spent_dict.items(), key=lambda x:x[1])
+print(f"{number} spent the longest time, {total_time} seconds, on the phone during September 2016.")
