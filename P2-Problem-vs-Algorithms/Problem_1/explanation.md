18001a066b47f20c4101d2b1d017464b5d800f9d
Biswas Biplob <Biplob.Biswas@burda-forward.de>
2020-07-07 15:24:56 +0200

    Add square root using newton raphson method

--- P2-Problem-vs-Algorithms/Problem_1/explanation.md
+++ P2-Problem-vs-Algorithms/Problem_1/explanation.md
@@ -1,7 +1,7 @@
-Using exponent (**) opeartor for power with 0.5 to calculate root
+Using Newton raphson method to initialize with a best guess and then repeat until you get the value. 
 
 Time Complexity:
-O(n) - For the calculation using the exponent operator
+O((logn)F(n)) - where n is the digit precision in the input number and F(n) is the cost of calculating f(x)/f'(x) with n-digit precision. Referred from - https://en.citizendium.org/wiki/Newton's_method#Computational_complexity
 
 Space Complexity:
 O(1) - No new space added
--- P2-Problem-vs-Algorithms/Problem_1/square-root-of-number.py
+++ P2-Problem-vs-Algorithms/Problem_1/square-root-of-number.py
@@ -1,17 +1,32 @@
 def sqrt(number):
-    """
+   
+   """
     Calculate the floored square root of a number
 
     Args:
        number(int): Number to find the floored squared root
     Returns:
        int: Floored Square Root
-    """
-    if number < 0 :
+   """
+   if number < 0 :
        print("Number must be greater than 0")
-       return None 
-    sqrt = number**(0.5)
-    return int(sqrt)
+       return None
+   
+   elif number in (0,1):
+      return number 
+
+   guess = float(number/2)
+   square_root = 0.0
+   i = 0
+   while (square_root != guess):
+      square_root =  (guess + (float(number)/guess)) * 0.5  # Using newton-raphson method 
+      if (square_root == guess):
+         return int(square_root)
+      else:
+         guess = square_root
+         square_root = 0.0
+
+   
 
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
@@ -20,4 +35,14 @@ print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
 print ("Pass" if  (5 == sqrt(35)) else "Fail")
 print ("Pass" if  (1 == sqrt(2)) else "Fail")
-print ("Pass" if  (1 == sqrt(-1)) else "Fail")
+print ("Pass\n" if  (None == sqrt(-1)) else "Fail\n")
+
+
+print ("Pass" if  (3 == sqrt_new(9)) else "Fail")
+print ("Pass" if  (0 == sqrt_new(0)) else "Fail")
+print ("Pass" if  (4 == sqrt_new(16)) else "Fail")
+print ("Pass" if  (1 == sqrt_new(1)) else "Fail")
+print ("Pass" if  (5 == sqrt_new(27)) else "Fail")
+print ("Pass" if  (5 == sqrt_new(35)) else "Fail")
+print ("Pass" if  (1 == sqrt_new(2)) else "Fail")
+print ("Pass" if  (None == sqrt_new(-1)) else "Fail")
